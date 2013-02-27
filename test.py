4b82adef1eceaecac8d3cd48e89e7e34af068b36
YuQing Zhang <zhangyuqing524@gmail.com>
2013-02-27 14:50:42 +0100

    Minor update for test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from SitesToAzureBlob import SitesToAzureBlob
 
 def main():
-    stb = SitesToAzureBlob('../yuqingzhang.com/src/main', '../yuqingzhang.com/output')
+    stb = SitesToAzureBlob('../yuqingzhang.com/src/main', '../yuqingzhang.com/output', overwrite_output = False, account_name = 'zhangyuqinglabs', account_key = 'zbC/2jangIEmAeKVE0c7KybQl8TwgEkGIK5tpnTJku0CUudDZR/mytqtJw2I/OiwKFZVMiVVWYKVjxMwpUQQbQ==',
+                         container_name = 'site')
     stb.upload_files_to_blob()
     
 
